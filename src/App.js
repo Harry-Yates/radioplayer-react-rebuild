2ef8790ab3ef257657f8f6ba06d2f456894d6fd3
Harry-Yates <hyates1@gmail.com>
2022-01-14 18:19:06 +0100

    Updatd environment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./styles/main.css";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>TeST</h1>
+    </div>
+  );
 }
 
 export default App;
