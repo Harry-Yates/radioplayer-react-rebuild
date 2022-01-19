f38613934731a68c9abdb437956b1ef817814048
Harry-Yates <hyates1@gmail.com>
2022-01-19 18:36:13 +0100

    Updates

---
+++ precache-manifest.5fc8ff8ebdfb5fe75e74f70d4de8ef3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbc34548a18f307de812e6d6e15ba52",
+    "url": "/radioplayer-react-rebuild/index.html"
+  },
+  {
+    "revision": "ca6e19d0ee9d9070162e",
+    "url": "/radioplayer-react-rebuild/static/css/main.0a62f1ea.chunk.css"
+  },
+  {
+    "revision": "1b14bd1e9a1276cccf5e",
+    "url": "/radioplayer-react-rebuild/static/js/2.e748ca02.chunk.js"
+  },
+  {
+    "revision": "ca6e19d0ee9d9070162e",
+    "url": "/radioplayer-react-rebuild/static/js/main.3c023000.chunk.js"
+  },
+  {
+    "revision": "3fe0ce12b783c0d9f9b7",
+    "url": "/radioplayer-react-rebuild/static/js/runtime~main.c7d8efb0.js"
+  },
+  {
+    "revision": "9742a7fc73792b092cbcbf37583dbcc8",
+    "url": "/radioplayer-react-rebuild/static/media/album.9742a7fc.jpg"
+  },
+  {
+    "revision": "9742a7fc73792b092cbcbf37583dbcc8",
+    "url": "/radioplayer-react-rebuild/static/media/albumCover.9742a7fc.jpg"
+  }
+]);
